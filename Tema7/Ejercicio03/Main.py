52b39d3a9612ed7f724b7891444b755a19c727ab
Japarraes <110259395+Japarraes@users.noreply.github.com>
2022-08-20 17:33:21 +0200

    Add files via upload

---
+++ Tema7/Ejercicio03/Main.py
@@ -0,0 +1,10 @@
+from Calculadora import Suma, Resta, Multiplica, Divide
+
+def main():
+    print(Suma.suma(3,1))
+    print(Resta.resta(9, 2))
+    print(Multiplica.multiplica(5, 5))
+    print(Divide.divide(81,9))
+
+if __name__ == "__main__":
+    main()
